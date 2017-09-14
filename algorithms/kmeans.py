2e09c138f9939229d5950ab8c9ae7e58da7a58be
luke-zhu <luke_zhu@brown.edu>
2017-09-14 11:30:24 -0400

    Updated lab 1b files

--- algorithms/kmeans.py
+++ algorithms/kmeans.py
@@ -1,19 +1,4 @@
 ##################
 # TASKS 9
 ##################
-def kmeans():
-    pass
 
-
-#################
-# Task 11
-#################
-def vectorized_kmeans():
-    pass
-
-
-###################
-# Task 12
-###################
-def parallelized_kmeans():
-    pass
---
+++ algorithms/kmeanspar.py
@@ -0,0 +1,11 @@
+###################
+# Task 12
+###################
+
+import numpy as np
+from random import sample
+import multiprocessing
+from multiprocessing import Pool
+num_cores = multiprocessing.cpu_count()
+
+
